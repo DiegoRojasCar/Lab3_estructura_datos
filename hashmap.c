04c4fd4d0396b268bee7e8dcf27af465a59821e1
Your Name <you@example.com>
2026-04-19 05:50:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -173,7 +173,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
 
-    if (map -> size == 0 || map -> current == -1){
+    if (map -> size == 0 || map -> current == -1 || map -> capacity == 1){
         return NULL;
     }
 
