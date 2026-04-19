5bed07f45158fb249aee33e551bbc38e7a780ee4
Your Name <you@example.com>
2026-04-19 02:41:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -62,7 +62,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     long index = hash(key, map -> capacity);
     Pair* newBucket = createPair(key, value); 
     
-    if(map -> buckets[index] == NULL){
+    if(map -> buckets[index] == NULL || map -> buckets[index] -> key == NULL){
         map -> buckets[index] = newBucket;
         map -> current = index;
         map -> capacity = map -> size + 1;
